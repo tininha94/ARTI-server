5c706fb5b94a545461bb278557fe109ceb17e305
tininha94 <c_ristina08@hotmail.com>
2023-06-20 09:24:21 -0300

    Change port to 8080

--- app.py
+++ app.py
@@ -97,4 +97,4 @@ api.add_resource(UploadFiles, '/')
 api.add_resource(GetResult, '/result') 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080)
